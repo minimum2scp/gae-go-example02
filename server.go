e53c9bf36fbba31f90e76b2eb79a1189e7a9a535
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2020-01-23 14:04:06 +0900

    go fmt

--- server.go
+++ server.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-  "net/http"
-  "os"
+	"net/http"
+	"os"
 
-  "github.com/labstack/echo/v4"
-  "github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-  // Echo instance
-  e := echo.New()
+	// Echo instance
+	e := echo.New()
 
-  // Middleware
-  e.Use(middleware.Logger())
-  e.Use(middleware.Recover())
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-  // Routes
-  e.GET("/", hello)
+	// Routes
+	e.GET("/", hello)
 
-  // Port
-  port := os.Getenv("PORT")
-  if port == "" {
-      port = "8080"
-  }
+	// Port
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-  // Start server
-  e.Logger.Fatal(e.Start(":" + port))
+	// Start server
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
 // Handler
 func hello(c echo.Context) error {
-  return c.String(http.StatusOK, "Hello, World!")
+	return c.String(http.StatusOK, "Hello, World!")
 }
