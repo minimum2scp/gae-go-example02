c5603b6b6ec38b6fb0743ff72b8da3100c18f1ea
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2020-01-23 13:48:58 +0900

    Added test

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module minimum2scp/gae-go-example02
 
-require github.com/labstack/echo/v4 v4.1.13 // indirect
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.1.13
+	github.com/stretchr/testify v1.4.0
+)
---
+++ server_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHello(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, hello(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "Hello, World!", rec.Body.String())
+	}
+}
